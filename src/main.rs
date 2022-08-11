20faf9619117b057a2a16d6d050ba10c3aa2a73c
Wai-Shing Luk <luk036@gmail.com>
2022-08-11 16:27:29 +0800

    try petgraph

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use petgraph::graph::Graph;
+
 fn main() {
-    println!("Hello, world!");
+    let mut graph = Graph::<&str, u32>::new();
+    let origin = graph.add_node("Denver");
+    let destination_1 = graph.add_node("San Diego");
+    let destination_2 = graph.add_node("New York");
+
+    graph.extend_with_edges(&[
+        (origin, destination_1, 250),
+        (origin, destination_2, 1099)
+    ]);
+
+    println!("{}", serde_json::to_string_pretty(&graph).unwrap());
+    // println!("Hello, world!");
 }
