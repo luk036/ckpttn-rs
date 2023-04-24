57f9ff0ffd3ed689e55fc07adae16568dd018544
Wai-Shing Luk <luk036@gmail.com>
2023-04-24 10:15:58 +0800

    apply clippy and fmt

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ fn main() {
     let destination_1 = graph.add_node("San Diego");
     let destination_2 = graph.add_node("New York");
 
-    graph.extend_with_edges(&[
-        (origin, destination_1, 250),
-        (origin, destination_2, 1099)
-    ]);
+    graph.extend_with_edges([(origin, destination_1, 250), (origin, destination_2, 1099)]);
 
     println!("{}", serde_json::to_string_pretty(&graph).unwrap());
     // println!("Hello, world!");
--- src/moveinfo.rs
+++ src/moveinfo.rs
@@ -2,12 +2,11 @@ pub struct MoveInfo<Node> {
     pub net: Node,
     pub v: Node,
     pub from_part: u8,
-    pub to_part: u8
+    pub to_part: u8,
 }
 
 pub struct MoveInfoV<Node> {
     pub v: Node,
     pub from_part: u8,
-    pub to_part: u8
-    // node_t v;
+    pub to_part: u8, // node_t v;
 }
