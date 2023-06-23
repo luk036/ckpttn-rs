587343d621c6e38275c9c521a05b83780aa550fa
Wai-Shing Luk <luk036@gmail.com>
2023-06-23 11:22:12 +0800

    add ai generated code

--- bpqueue_ai.rs
+++ bpqueue_ai.rs
@@ -1,73 +1,178 @@
-use std::collections::LinkedList;
-type Item = LinkedList<Vec<i32>>;
-const SENTINEL: Item = LinkedList::new();
-struct BPQueue {
-    _bucket: Vec<LinkedList<Vec<i32>>>,
+use crate::dllist::{Dllink, Dllist};
+
+// Define a static variable SENTINEL of type Dllink
+static SENTINEL: Dllink = Dllink { data: [0, 8965], prev: std::ptr::null_mut(), next: std::ptr::null_mut() };
+
+// Define a struct BPQueue
+pub struct BPQueue {
     _max: usize,
     _offset: i32,
-    _high: i32,
+    _high: usize,
+    _bucket: Vec<Dllist>,
 }
+
 impl BPQueue {
-    fn new(a: i32, b: i32) -> Self {
+    // Define a new function for BPQueue
+    pub fn new(a: i32, b: i32) -> Self {
+        // Check if a is less than or equal to b
         assert!(a <= b);
+        // Define offset as a - 1
         let offset = a - 1;
-        let high = b - offset;
-        let mut bucket = Vec::with_capacity((high + 1) as usize);
-        for i in 0..=high {
-            bucket.push(LinkedList::new());
-            bucket[i as usize].push_back(vec![i as i32, 4848]);
-        }
-        bucket[0].push_back(SENTINEL);
-        BPQueue {
-            _bucket: bucket,
+        // Define high as b - offset
+        let high = (b - offset) as usize;
+        // Define bucket as a vector of Dllist of size high + 1
+        let mut bucket = vec![Dllist::new(); high + 1];
+        // Append SENTINEL to the first element of bucket
+        bucket[0].append(&mut SENTINEL.clone());
+        // Return a new instance of BPQueue
+        Self {
             _max: 0,
             _offset: offset,
             _high: high,
+            _bucket: bucket,
         }
     }
-    fn clear(&mut self) {
+
+    // Define a function to check if BPQueue is empty
+    pub fn is_empty(&self) -> bool {
+        self._max == 0
+    }
+
+    // Define a function to get the maximum value in BPQueue
+    pub fn get_max(&self) -> i32 {
+        (self._max as i32) + self._offset
+    }
+
+    // Define a function to clear BPQueue
+    pub fn clear(&mut self) {
         while self._max > 0 {
             self._bucket[self._max].clear();
             self._max -= 1;
         }
     }
-    fn append(&mut self, mut it: Item, k: i32) {
-        assert!(k > self._offset);
-        it.front_mut().unwrap()[0] = k - self._offset;
-        if self._max < it.front().unwrap()[0] as usize {
-            self._max = it.front().unwrap()[0] as usize;
-        }
-        self._bucket[it.front().unwrap()[0] as usize].push_back(it);
+
+    // Define a function to set the key of an item in BPQueue
+    pub fn set_key(&self, it: &mut Dllink, gain: i32) {
+        it.data[0] = gain - self._offset;
     }
-}
-impl IntoIterator for BPQueue {
-    type Item = Item;
-    type IntoIter = BPQueueIterator;
-    fn into_iter(self) -> Self::IntoIter {
+
+    // Define a function to append an item directly to BPQueue
+    pub fn append_direct(&mut self, it: &mut Dllink) {
+        assert!(it.data[0] > self._offset);
+        self.append(it, it.data[0]);
+    }
+
+    // Define a function to append an item to BPQueue
+    pub fn append(&mut self, it: &mut Dllink, k: i32) {
+        assert!(k > self._offset);
+        it.data[0] = k - self._offset;
+        if self._max < it.data[0] as usize {
+            self._max = it.data[0] as usize;
+        }
+        self._bucket[it.data[0] as usize].append(it);
+    }
+
+    // Define a function to append a vector of items to BPQueue
+    pub fn appendfrom(&mut self, nodes: &mut Vec<Dllink>) {
+        for it in nodes.iter_mut() {
+            it.data[0] -= self._offset;
+            assert!(it.data[0] > 0);
+            self._bucket[it.data[0] as usize].append(it);
+        }
+        self._max = self._high;
+        while self._bucket[self._max].is_empty() {
+            self._max -= 1;
+        }
+    }
+
+    // Define a function to remove and return the leftmost item in BPQueue
+    pub fn popleft(&mut self) -> Dllink {
+        let res = self._bucket[self._max].popleft();
+        while self._bucket[self._max].is_empty() {
+            self._max -= 1;
+        }
+        res
+    }
+
+    // Define a function to decrease the key of an item in BPQueue
+    pub fn decrease_key(&mut self, it: &mut Dllink, delta: i32) {
+        it.detach();
+        it.data[0] += delta;
+        assert!(it.data[0] > 0);
+        assert!(it.data[0] <= self._high as i32);
+        self._bucket[it.data[0] as usize].append(it);
+        if self._max < it.data[0] as usize {
+            self._max = it.data[0] as usize;
+            return;
+        }
+        while self._bucket[self._max].is_empty() {
+            self._max -= 1;
+        }
+    }
+
+    // Define a function to increase the key of an item in BPQueue
+    pub fn increase_key(&mut self, it: &mut Dllink, delta: i32) {
+        it.detach();
+        it.data[0] += delta;
+        assert!(it.data[0] > 0);
+        assert!(it.data[0] <= self._high as i32);
+        self._bucket[it.data[0] as usize].appendleft(it);
+        if self._max < it.data[0] as usize {
+            self._max = it.data[0] as usize;
+        }
+    }
+
+    // Define a function to modify the key of an item in BPQueue
+    pub fn modify_key(&mut self, it: &mut Dllink, delta: i32) {
+        if it.next.is_null() {
+            return;
+        }
+        if delta > 0 {
+            self.increase_key(it, delta);
+        } else if delta < 0 {
+            self.decrease_key(it, delta);
+        }
+    }
+
+    // Define a function to detach an item from BPQueue
+    pub fn detach(&mut self, it: &mut Dllink) {
+        it.detach();
+        while self._bucket[self._max].is_empty() {
+            self._max -= 1;
+        }
+    }
+
+    // Define a function to iterate over BPQueue
+    pub fn iter(&self) -> BPQueueIterator {
         BPQueueIterator {
             bpq: self,
-            curkey: self._max as i32,
-            curitem: self.bpq._bucket[self.bpq._max].iter(),
+            curkey: self._max,
+            curitem: self._bucket[self._max].iter(),
         }
     }
 }
-struct BPQueueIterator {
-    bpq: BPQueue,
-    curkey: i32,
-    curitem: std::collections::linked_list::Iter<'static, Vec<i32>>,
+
+// Define a struct BPQueueIterator
+pub struct BPQueueIterator<'a> {
+    bpq: &'a BPQueue,
+    curkey: usize,
+    curitem: std::slice::Iter<'a, Dllink>,
 }
-impl Iterator for BPQueueIterator {
-    type Item = Item;
+
+impl<'a> Iterator for BPQueueIterator<'a> {
+    type Item = &'a Dllink;
+
     fn next(&mut self) -> Option<Self::Item> {
         while self.curkey > 0 {
             match self.curitem.next() {
-                Some(res) => return Some(res.clone()),
+                Some(res) => return Some(res),
                 None => {
                     self.curkey -= 1;
-                    self.curitem = self.bpq._bucket[self.curkey as usize].iter();
+                    self.curitem = self.bpq._bucket[self.curkey].iter();
                 }
             }
         }
         None
     }
 }
+
--- dllist_ai.rs
+++ dllist_ai.rs
@@ -1,44 +1,60 @@
 struct Dllink<T> {
-    next: Option<Box<Dllink<T>>>,
+    next: Option<*mut Dllink<T>>,
     prev: Option<*mut Dllink<T>>,
     data: T,
 }
 
 impl<T> Dllink<T> {
     fn new(data: T) -> Self {
-        Dllink {
+        Self {
             next: None,
             prev: None,
             data,
         }
     }
 
-    fn appendleft(&mut self, node: &mut Dllink<T>) {
-        node.next = self.next.take();
-        node.prev = Some(self as *mut Dllink<T>);
-        if let Some(ref mut next) = node.next {
-            next.prev = Some(node);
-        }
-        self.next = Some(Box::new(node));
+    fn is_locked(&self) -> bool {
+        self.next.is_none()
     }
-}
 
-struct DllIterator<'a, T> {
-    link: &'a Dllink<T>,
-    cur: Option<&'a Dllink<T>>,
-}
+    fn lock(&mut self) {
+        self.next = None;
+    }
 
-impl<'a, T> Iterator for DllIterator<'a, T> {
-    type Item = &'a Dllink<T>;
+    fn appendleft(&mut self, node: &mut Dllink<T>) {
+        node.next = self.next;
+        self.next.unwrap().prev = Some(node);
+        self.next = Some(node);
+        node.prev = Some(self);
+    }
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(cur) = self.cur.take() {
-            if cur as *const Dllink<T> != self.link as *const Dllink<T> {
-                self.cur = cur.next.as_deref();
-                return Some(cur);
-            }
-        }
-        None
+    fn append(&mut self, node: &mut Dllink<T>) {
+        node.prev = self.prev;
+        self.prev.unwrap().next = Some(node);
+        self.prev = Some(node);
+        node.next = Some(self);
+    }
+
+    fn popleft(&mut self) -> &mut Dllink<T> {
+        let res = self.next.unwrap();
+        self.next = res.next;
+        self.next.unwrap().prev = Some(self);
+        unsafe { &mut *res }
+    }
+
+    fn pop(&mut self) -> &mut Dllink<T> {
+        let res = self.prev.unwrap();
+        self.prev = res.prev;
+        self.prev.unwrap().next = Some(self);
+        unsafe { &mut *res }
+    }
+
+    fn detach(&mut self) {
+        assert!(self.next.is_some());
+        let n = self.next.unwrap();
+        let p = self.prev.unwrap();
+        p.next = Some(n);
+        n.prev = Some(p);
     }
 }
 
@@ -48,67 +64,94 @@ struct Dllist<T> {
 
 impl<T> Dllist<T> {
     fn new(data: T) -> Self {
-        Dllist {
+        Self {
             head: Dllink::new(data),
         }
-    }
-
-    fn appendleft(&mut self, node: &mut Dllink<T>) {
-        self.head.appendleft(node);
-    }
-
-    fn pop(&mut self) -> Option<Box<Dllink<T>>> {
-        self.head.next.take().map(|mut node| {
-            self.head.next = node.next.take();
-            if let Some(ref mut next) = node.next {
-                next.prev = Some(&mut self.head);
-            }
-            node
-        })
-    }
-
-    fn popleft(&mut self) -> Option<Box<Dllink<T>>> {
-        self.head.next.take().map(|mut node| {
-            self.head.next = node.next.take();
-            if let Some(ref mut next) = node.next {
-                next.prev = Some(&mut self.head);
-            }
-            node
-        })
     }
 
     fn is_empty(&self) -> bool {
         self.head.next.is_none()
     }
 
-    fn iter(&self) -> DllIterator<T> {
-        DllIterator {
-            link: &self.head,
-            cur: self.head.next.as_deref(),
+    fn clear(&mut self) {
+        self.head.next = None;
+        self.head.prev = None;
+    }
+
+    fn appendleft(&mut self, node: &mut Dllink<T>) {
+        self.head.appendleft(node);
+    }
+
+    fn append(&mut self, node: &mut Dllink<T>) {
+        self.head.append(node);
+    }
+
+    fn popleft(&mut self) -> &mut Dllink<T> {
+        self.head.popleft()
+    }
+
+    fn pop(&mut self) -> &mut Dllink<T> {
+        self.head.pop()
+    }
+}
+
+struct DllIterator<'a, T> {
+    link: *mut Dllink<T>,
+    cur: *mut Dllink<T>,
+    marker: std::marker::PhantomData<&'a mut Dllist<T>>,
+}
+
+impl<'a, T> DllIterator<'a, T> {
+    fn new(head: &'a mut Dllink<T>) -> Self {
+        Self {
+            link: head as *mut Dllink<T>,
+            cur: head.next.unwrap() as *mut Dllink<T>,
+            marker: std::marker::PhantomData,
         }
     }
 }
 
-fn test_dllink() {
-    let mut L1 = Dllist::new(99);
-    let mut L2 = Dllist::new(99);
-    let mut d = Box::new(Dllink::new(1));
-    let mut e = Box::new(Dllink::new(2));
-    let mut f = Box::new(Dllink::new(3));
-    L1.appendleft(&mut e);
-    assert!(!L1.is_empty());
-    L1.appendleft(&mut f);
-    L1.append(&mut d);
-    L2.append(L1.pop().unwrap());
-    L2.append(L1.popleft().unwrap());
-    assert!(!L1.is_empty());
-    assert!(!L2.is_empty());
-    e.detach();
-    assert!(L1.is_empty());
-    let mut count = 0;
-    for _ in L2.iter() {
-        count += 1;
+impl<'a, T> Iterator for DllIterator<'a, T> {
+    type Item = &'a mut Dllink<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur == self.link {
+            return None;
+        }
+        let res = unsafe { &mut *self.cur };
+        self.cur = res.next.unwrap() as *mut Dllink<T>;
+        Some(res)
     }
-    assert_eq!(count, 2);
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dllink() {
+        let mut l1 = Dllist::new(());
+        let mut l2 = Dllist::new(());
+        let mut d = Dllink::new(());
+        let mut e = Dllink::new(());
+        let mut f = Dllink::new(());
+
+        l1.appendleft(&mut e);
+        assert!(!l1.is_empty());
+
+        l1.appendleft(&mut f);
+        l1.append(&mut d);
+        l2.append(l1.pop());
+        l2.append(l1.popleft());
+        assert!(!l1.is_empty());
+        assert!(!l2.is_empty());
+        e.detach();
+        assert!(l1.is_empty());
+
+        let mut count = 0;
+        for _ in DllIterator::new(&mut l2.head) {
+            count += 1;
+        }
+        assert_eq!(count, 2);
+    }
+}
